7f1fd02f421e4ba8f64a41f2a5218c1e835654fb
Megan Coyle <mcoyle@usgbc.org>
2017-05-30 15:17:55 -0400

    Move navigation to a function

--- public/manage_content.php
+++ public/manage_content.php
@@ -18,47 +18,7 @@
 
     <div id="main">
       <div id="navigation">
-        <ul class="subjects">
-          <?php $subject_set = find_all_subjects(); ?>
-    		<?php
-    			while($subject = mysqli_fetch_assoc($subject_set)) {
-    		?>
-    				<?php
-              echo "<li";
-              if ($subject["id"] == $selected_subject_id) {
-                echo " class=\"selected\"";
-              }
-              echo ">";
-            ?>
-              <a href="manage_content.php?subject=<?php echo urlencode($subject['id']); ?>"><?php echo $subject["menu_name"]; ?></a>
-              <?php $page_set = find_pages_for_subject($subject['id']); ?>
-              <ul class="pages">
-                <?php
-            			while($page = mysqli_fetch_assoc($page_set)) {
-            		?>
-                <?php
-                  echo "<li";
-                  if ($page["id"] == $selected_page_id) {
-                    echo " class=\"selected\"";
-                  }
-                  echo ">";
-                ?>
-                      <a href="manage_content.php?page=<?php echo urlencode($page['id']); ?>"><?php echo $page["menu_name"]; ?></a>
-                    </li>
-                  <?php
-                		}
-                	?>
-                  <?php mysqli_free_result($page_set); ?>
-              </ul>
-            </li>
-    	  <?php
-    			}
-    		?>
-        <?php
-        // release returned data
-          mysqli_free_result($subject_set);
-        ?>
-    		</ul>
+        <?php echo navigation($selected_subject_id, $selected_page_id); ?>
       </div>
       <div id="page">
         <h2>Manage Content</h2>
