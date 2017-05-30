521472f13af9f544339976077b5e30f53b3e57c2
Megan Coyle <mcoyle@usgbc.org>
2017-05-30 15:55:34 -0400

    Finding a subject in the database

--- includes/functions.php
+++ includes/functions.php
@@ -1,42 +1,99 @@
 <?php
 
-  function confirm_query($result_set) {
-    if (!$result_set) {
-      die("Database query failed.");
-    }
-  }
+	function confirm_query($result_set) {
+		if (!$result_set) {
+			die("Database query failed.");
+		}
+	}
 
-  function find_all_subjects () {
-    // perform database query
-    global $connection;
-    $query  = "SELECT * ";
-    $query .= "FROM subjects ";
-    $query .= "WHERE visible = 1 ";
-    $query .= "ORDER BY position ASC";
-    $subject_set = mysqli_query($connection, $query);
-    confirm_query($subject_set);
-    return $subject_set;
-  }
+	function find_all_subjects() {
+		global $connection;
 
-  function find_pages_for_subject($subject_id) {
-    global $connection;
-    $query  = "SELECT * ";
-    $query .= "FROM pages ";
-    $query .= "WHERE visible = 1 ";
-    $query .= "AND subject_id = {$subject_id} ";
-    $query .= "ORDER BY position ASC";
-    $page_set = mysqli_query($connection, $query);
-    confirm_query($page_set);
-    return $page_set;
-  }
+		$query  = "SELECT * ";
+		$query .= "FROM subjects ";
+		// $query .= "WHERE visible = 1 ";
+		$query .= "ORDER BY position ASC";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		return $subject_set;
+	}
 
-  // navigation takes 2 arguments, currently selected id (if any) and currently selected page id (if any)
-  function navigation($subject_id, $page_id) {
-    $output = "<ul class=\"subjects\">";
+	function find_pages_for_subject($subject_id) {
+		global $connection;
+
+		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
+		$query  = "SELECT * ";
+		$query .= "FROM pages ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "AND subject_id = {$safe_subject_id} ";
+		$query .= "ORDER BY position ASC";
+		$page_set = mysqli_query($connection, $query);
+		confirm_query($page_set);
+		return $page_set;
+	}
+
+	function find_subject_by_id($subject_id) {
+		global $connection;
+
+		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
+		$query  = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE id = {$safe_subject_id} ";
+		$query .= "LIMIT 1";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		if($subject = mysqli_fetch_assoc($subject_set)) {
+			return $subject;
+		} else {
+			return null;
+		}
+	}
+
+	function find_page_by_id($page_id) {
+		global $connection;
+
+		$safe_page_id = mysqli_real_escape_string($connection, $page_id);
+
+		$query  = "SELECT * ";
+		$query .= "FROM pages ";
+		$query .= "WHERE id = {$safe_page_id} ";
+		$query .= "LIMIT 1";
+		$page_set = mysqli_query($connection, $query);
+		confirm_query($page_set);
+		if($page = mysqli_fetch_assoc($page_set)) {
+			return $page;
+		} else {
+			return null;
+		}
+	}
+
+	function find_selected_page() {
+		global $current_subject;
+		global $current_page;
+
+		if (isset($_GET["subject"])) {
+			$current_subject = find_subject_by_id($_GET["subject"]);
+			$current_page = null;
+		} elseif (isset($_GET["page"])) {
+			$current_subject = null;
+			$current_page = find_page_by_id($_GET["page"]);
+		} else {
+			$current_subject = null;
+			$current_page = null;
+		}
+	}
+
+	// navigation takes 2 arguments
+	// - the current subject array or null
+	// - the current page array or null
+	function navigation($subject_array, $page_array) {
+		$output = "<ul class=\"subjects\">";
 		$subject_set = find_all_subjects();
 		while($subject = mysqli_fetch_assoc($subject_set)) {
 			$output .= "<li";
-			if ($subject["id"] == $subject_id) {
+			if ($subject_array && $subject["id"] == $subject_array["id"]) {
 				$output .= " class=\"selected\"";
 			}
 			$output .= ">";
@@ -50,7 +107,7 @@
 			$output .= "<ul class=\"pages\">";
 			while($page = mysqli_fetch_assoc($page_set)) {
 				$output .= "<li";
-				if ($page["id"] == $page_id) {
+				if ($page_array && $page["id"] == $page_array["id"]) {
 					$output .= " class=\"selected\"";
 				}
 				$output .= ">";
@@ -66,6 +123,6 @@
 		mysqli_free_result($subject_set);
 		$output .= "</ul>";
 		return $output;
-  }
+	}
 
 ?>
