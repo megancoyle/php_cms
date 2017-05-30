d354d948f286b8c932433e687696f67f4ba01a03
Megan Coyle <mcoyle@usgbc.org>
2017-05-30 14:55:41 -0400

    Highlight the current page in navigation

--- public/manage_content.php
+++ public/manage_content.php
@@ -3,6 +3,19 @@
 
 <?php include("../includes/layouts/header.php"); ?>
 
+<?php
+  if (isset($_GET["subject"])) {
+    $selected_subject_id = $_GET["subject"];
+    $selected_page_id = null;
+  } elseif (isset($_GET["page"])) {
+    $selected_page_id = $_GET["page"];
+    $selected_subject_id = null;
+  } else {
+    $selected_subject_id = null;
+    $selected_page_id = null;
+  }
+?>
+
     <div id="main">
       <div id="navigation">
         <ul class="subjects">
@@ -10,14 +23,28 @@
     		<?php
     			while($subject = mysqli_fetch_assoc($subject_set)) {
     		?>
-    				<li>
-              <?php echo $subject["menu_name"]; ?>
+    				<?php
+              echo "<li";
+              if ($subject["id"] == $selected_subject_id) {
+                echo " class=\"selected\"";
+              }
+              echo ">";
+            ?>
+              <a href="manage_content.php?subject=<?php echo urlencode($subject['id']); ?>"><?php echo $subject["menu_name"]; ?></a>
               <?php $page_set = find_pages_for_subject($subject['id']); ?>
               <ul class="pages">
                 <?php
             			while($page = mysqli_fetch_assoc($page_set)) {
             		?>
-            				<li><?php echo $page["menu_name"]; ?></li>
+                <?php
+                  echo "<li";
+                  if ($page["id"] == $selected_page_id) {
+                    echo " class=\"selected\"";
+                  }
+                  echo ">";
+                ?>
+                      <a href="manage_content.php?page=<?php echo urlencode($page['id']); ?>"><?php echo $page["menu_name"]; ?></a>
+                    </li>
                   <?php
                 		}
                 	?>
@@ -35,6 +62,8 @@
       </div>
       <div id="page">
         <h2>Manage Content</h2>
+        <?php echo $selected_subject_id; ?><br>
+        <?php echo $selected_page_id; ?>
       </div>
     </div>
 
