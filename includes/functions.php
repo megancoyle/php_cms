a5d47b979b45fc9c7e6b2e3e04504156d4c793e8
Megan Coyle <mcoyle@usgbc.org>
2017-06-01 09:07:55 -0400

    Add authentication for admins

--- includes/functions.php
+++ includes/functions.php
@@ -317,4 +317,14 @@
 			return false;
 		}
 	}
+	
+	function logged_in() {
+		return isset($_SESSION['admin_id']);
+	}
+
+	function confirm_logged_in() {
+		if (!logged_in()) {
+	    redirect_to("login.php");
+	  }
+	}
 ?>
